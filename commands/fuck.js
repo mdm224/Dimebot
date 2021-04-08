6617a2742231fe1a5c56b9dc4b7e21deb80b7f89
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-04-08 19:48:34 -0400

    Update fuck.js

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'fuck',
-    description: 'this is a addquote command',
+    description: 'this is a meme command',
     execute(message)
     {
         message.channel.send("https://tenor.com/view/lars-ulrich-metallica-fuck-gif-8302504");

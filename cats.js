f30f4026211442ba7eec47f83ab46a7755c1e215
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-04-08 19:47:23 -0400

    Delete cats.js

--- cats.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer-extra');
-
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://www.metal-archives.com/images/cats/1.jpg');
-    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: 'C:\Users\Administrator\Desktop\cats\cat1.jpg'});
-})();
-

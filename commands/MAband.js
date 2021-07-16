87d1ed17c54e3ff295a9dfb29eb13b0d3187f407
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-07-15 22:47:07 -0400

    Update MAband.js

--- commands/MAband.js
+++ commands/MAband.js
@@ -7,23 +7,42 @@ module.exports = {
         
         (async () => {
           
-          const browser = await puppeteer.launch({
-                  headless: true,
-                  args: ['--no-sandbox','--disable-setuid-sandbox']
-                });
+          const browser = await puppeteer.launch();
           const page = await browser.newPage();
           await page.goto('https://www.metal-archives.com/bands/'+bandName);
           const url = await page.url();
+        
+          try {
+              await page.waitForSelector ("body > div > div > div > div > div > dl.float_right > dd", { timeout: 500 })
+              const result1s = await google.scrape(bandName + " metallum", 1);
+            console.log('results', result1s);
+              message.channel.send({ embed: {
+            color: 3447003,
+            title: "Band name: "+ bandName,
+            image: {
+                url: result1s[0].url},
+            url: url}});
+            }
+         catch (error) {
+                try{
+                    await page.waitForSelector ("#content_wrapper > ul", { timeout: 1000 })
+                    const result1s = await google.scrape(bandName + " metallum", 1);
+                    console.log('results', result1s);
+                    message.channel.send({ embed: {
+                    color: 3447003,
+                    title: "Band name: "+ bandName,
+                    image: {
+                        url: result1s[0].url},
+                    url: url}});
+                }
 
-          const result1s = await google.scrape(bandName + " metallum", 1);
-          console.log('results', result1s);
+                
+                catch (error) {
+                message.channel.send("Not on MA.");
+                }
+            }
           await browser.close();
-          message.channel.send({ embed: {
-              color: 3447003,
-              title: "Band name: "+ bandName,
-              image: {
-                  url: result1s[0].url},
-              url: url}});
         })();
-    }
+    
+}
 }

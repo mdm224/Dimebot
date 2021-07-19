d1a7fa47b7f3ee10db34cac10c0a12d1a288cf09
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-07-18 23:08:17 -0400

    Add files via upload

---
+++ commands/giverole.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: 'giverole',
+    permissions: "ADMINISTRATOR",
+    execute(message, args){
+
+        const targetUser = message.mentions.users.first()
+        if (!targetUser)
+        {
+            message.reply("Please specify.")
+            return
+        }
+
+        args.shift();
+
+        const roleName = args.join(' ');
+
+        const { guild } = message;
+
+        const role = guild.roles.cache.find((role)=>{
+            return role.name === roleName;
+        })
+            if (!role){
+                message.reply("Does not exist.");
+                return
+            }
+
+            const member = guild.members.cache.get(targetUser.id);
+            member.roles.add(role);
+
+            message.channel.send("Done!");
+    }
+}
---
+++ commands/removeRole.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: 'removerole',
+    permissions: "ADMINISTRATOR",
+    execute(message, args){
+
+        const targetUser = message.mentions.users.first()
+        if (!targetUser)
+        {
+            message.reply("Please specify.")
+            return
+        }
+
+        args.shift();
+
+        const roleName = args.join(' ');
+
+        const { guild } = message;
+
+        const role = guild.roles.cache.find((role)=>{
+            return role.name === roleName;
+        })
+            if (!role){
+                message.reply("Does not exist.");
+                return
+            }
+
+            const members = guild.members.cache.get(targetUser.id);
+            if (members.roles.cache.get(role.id)){
+            members.roles.remove(role);
+            }
+            else {
+                message.reply("They don't have this role.");
+            }
+            message.channel.send("Done!");
+    }
+}

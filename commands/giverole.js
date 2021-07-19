477859e0bea2876c4859ea5522049db2689da0a6
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-07-18 23:32:17 -0400

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -2,33 +2,35 @@ module.exports = {
     name: 'giverole',
     permissions: "ADMINISTRATOR",
     execute(message, args){
-        if (member.hasPermission(['MANAGE_ROLES'])) {
+        
 
-        const targetUser = message.mentions.users.first()
-        if (!targetUser)
-        {
-            message.reply("Please specify.")
-            return
-        }
 
-        args.shift();
-
-        const roleName = args.join(' ');
-
-        const { guild } = message;
-
-        const role = guild.roles.cache.find((role)=>{
-            return role.name === roleName;
-        })
-            if (!role){
-                message.reply("Does not exist.");
+            const targetUser = message.mentions.users.first()
+            if (!targetUser)
+            {
+                message.reply("Please specify.")
                 return
             }
 
-            const member = guild.members.cache.get(targetUser.id);
-            member.roles.add(role);
+            args.shift();
 
-            message.channel.send("Done!");
+            const roleName = args.join(' ');
+
+            const { guild } = message;
+
+            const role = guild.roles.cache.find((role)=>{
+                return role.name === roleName;
+            })
+                if (!role){
+                    message.reply("Does not exist.");
+                    return
+                }
+
+                const member = guild.members.cache.get(targetUser.id);
+                if (member.hasPermission(['MANAGE_ROLES'])) {
+                member.roles.add(role);
+
+                message.channel.send("Done!");
     }
     else{
         message.channel.send("You can't remove roles.");

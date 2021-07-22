2efd917d2c87acf3ccdb388f649b50d032805b0c
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-07-21 20:16:59 -0400

    Update createRole.js

--- commands/createRole.js
+++ commands/createRole.js
@@ -3,14 +3,21 @@ const { User } = require("discord.js");
 module.exports = {
     name: 'createrole',
     permissions: "ADMINISTRATOR",
-    execute(message, args){
+    execute(message, args, prefix){
 
+        args = args.replace("createrole", "");
+        args = args.slice(prefix.length).trim().split(',');
         const name = args[0];
+        console.log(name);
+        args = args.toString();
+        args = args.replace(/ /g, "");
+        console.log(args);
+        args = args.split(',');
         var color = args[1];
         color = color.toUpperCase();
 
 
-        let guild = message.guild;
+       
        
         if (message.member.hasPermission(['MANAGE_ROLES'])) {
         message.guild.roles.create({
@@ -25,7 +32,7 @@ module.exports = {
 
     message.channel.send("Created role " + name + " with the color "+ color);
 }
-else {
+else{
     message.channel.send("You don't have the perms to create a new role.");
 }
 }}

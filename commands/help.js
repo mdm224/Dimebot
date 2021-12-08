916b4cbb713c414a920993cbde836e60e59f9c26
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 15:32:17 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,7 @@ module.exports = {
     execute(message)
     {
         message.channel.send("```Command list: "+ 
-        "metal - provides the Metal-Archives link of the provided band\n"+
-        "img - provides a random image of the args provided\n"+
-        "pick - picks a random number from 0 to the provided number\n"+
-        "randbandc - returns a random band from MA based on hte country provided"+
+        "randbandc - returns a random band from MA based on the country provided"+
         "randband - provides a random band of the specificed genre from Metal-Archives\n"+
         "createRole - creates a new role with the name provided"+
         "addrole - adds the selected role to the provided user"+

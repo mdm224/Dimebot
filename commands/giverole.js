826684597aff8cbef8d15fed0395a55cb7a1f0e9
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 16:34:57 -0500

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -36,3 +36,42 @@ module.exports = {
     }
 }
 }
+
+
+
+}
+function removeTempRole (message, args) {
+
+    const targetUser = message.mentions.users.first()
+    if (!targetUser)
+    {
+        message.reply("Please specify.")
+        return
+    }
+
+    args.shift();
+
+    const roleName = args.join(' ');
+
+    const { guild } = message;
+
+    const role = guild.roles.cache.find((role)=>{
+        return role.name === roleName;
+    })
+        if (!role){
+            message.reply("Does not exist.");
+            return
+        }
+
+        const members = guild.members.cache.get(targetUser.id);
+        if (message.member.hasPermission(['MANAGE_ROLES'])) {
+        if (members.roles.cache.get(role.id)){
+        members.roles.remove(role);
+        }
+        
+        message.channel.send("Done! User no longer has this role.");
+    
+    
+
+}
+}

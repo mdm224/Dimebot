a17d99e4c07229867035945c1ff5fd0d1b693fdd
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 15:17:41 -0500

    Delete fuck.js

--- commands/fuck.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'fuck',
-    description: 'this is a meme command',
-    execute(message)
-    {
-        message.channel.send("https://tenor.com/view/lars-ulrich-metallica-fuck-gif-8302504");
-    }
-}

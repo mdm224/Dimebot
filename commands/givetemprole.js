8f2ba98ba97a306f2a87a9c1a534f9dd7fa51cc5
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 18:08:08 -0500

    Create givetemprole.js

---
+++ commands/givetemprole.js
@@ -0,0 +1,45 @@
+module.exports = {
+    name: 'givetemprole',
+    permissions: "ADMINISTRATOR",
+    execute(message, args){
+
+            const targetUser = message.mentions.users.first()
+            if (!targetUser)
+            {
+                message.reply("Please specify.")
+                return
+            }
+
+            args.shift();
+
+            const roleName = args.join(' ');
+
+            const { guild } = message;
+
+            const role = guild.roles.cache.find((role)=>{
+                return role.name === roleName;
+            })
+                if (!role){
+                    message.reply("Does not exist.");
+                    return
+                }
+
+                const member = guild.members.cache.get(targetUser.id);
+                if (message.member.hasPermission(['MANAGE_ROLES'])) {
+                member.roles.add(role);
+
+                message.channel.send("Done! User now has this role.");
+                var interval = setInterval(function() {
+                member.roles.remove(role);
+        
+                message.channel.send("Done! User no longer has this role.");
+                clearInterval(interval);
+                }, 5000);
+               // var interval = setInterval(removeTempRole, 500, targetUser, role, guild, message, interval, member);
+                
+    }
+    else{
+        message.channel.send("You can't remove roles.");
+    }
+}
+}

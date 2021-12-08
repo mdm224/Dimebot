90490ac11f112d4caf23d0517779d906872268f3
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 15:18:12 -0500

    Delete hetfield.js

--- commands/hetfield.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'hetfield',
-    description: 'this is a addquote command',
-    execute(message)
-    {
-        message.channel.send("Gimme fuel gimme fire gimme dabdabadire. OOOOOOOOOO YEAAAAAAAAAH");
-        message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/007/340/Screenshot_3.png");
-    }
-}

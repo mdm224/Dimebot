c4f237989db7e4cbb786918dac948f1b7246a6fd
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 15:15:54 -0500

    Delete quotelist.js

--- commands/quotelist.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'quotelist',
-    description: 'this is a quotelist command',
-    execute(message)
-    {
-        message.channel.send("https://docs.google.com/document/d/1C0ApFGHV6hToq-1OjTpbl-2KOCJ7Ao0pCex5wRZq1JQ/edit?usp=sharing");
-    }
-}

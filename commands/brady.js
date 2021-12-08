3dfd78c4071769a462fdc257af30a85988bf9c36
mdm224 <80916664+mdm224@users.noreply.github.com>
2021-12-08 15:17:07 -0500

    Delete brady.js

--- commands/brady.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'brady',
-    description: 'this is a command to annoy jon',
-    execute(message)
-    {
-        message.channel.send("https://tenor.com/view/tom-brady-zoom-patriots-cheating-close-up-gif-16190044");
-    }
-}
